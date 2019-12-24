a6e8cf38c1745243a9704bfba389aa670ab84207
Jordan Tangy <jordantangy@gmail.com>
2019-12-24 19:13:56 +0200

    Implementing Edge class

--- src/dataStructure/Edge.java
+++ src/dataStructure/Edge.java
@@ -1,47 +1,69 @@
 package dataStructure;
 
 public class Edge implements edge_data {
-
+	private double weight;
+	private Node src;
+	private Node dest;
+	private String info;
+	private int tag;
+	
+	
+	public Edge() {
+		this.src = null;
+		this.dest = null;
+		this.weight = 0;
+		this.info = "";
+		this.tag = 0;
+	}
+	
+	
+	public Edge(Node src, Node dest, double weight) {
+		this.src= src;
+		this.dest = dest;
+		this.weight = weight;
+		
+	}
+	
+	
 	@Override
 	public int getSrc() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.src.getKey();
 	}
 
+	
 	@Override
 	public int getDest() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.dest.getKey();
 	}
 
+	
 	@Override
 	public double getWeight() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.weight;
 	}
 
+	
 	@Override
 	public String getInfo() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.info;
 	}
 
+	
 	@Override
 	public void setInfo(String s) {
-		// TODO Auto-generated method stub
-		
+		this.info = s;
 	}
 
+	
 	@Override
 	public int getTag() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.tag;
 	}
 
+	
 	@Override
 	public void setTag(int t) {
-		// TODO Auto-generated method stub
-		
+		this.tag = t;
 	}
 
 }
