5818cd054ecace3c01efad56e46ff7e532cc1c7c
Yarin Shapira <yariniii19@gmail.com>
2019-12-24 16:30:15 +0200

    adding getV getE removeNode removeEdge nodeSize

--- src/dataStructure/DGraph.java
+++ src/dataStructure/DGraph.java
@@ -1,25 +1,36 @@
 package dataStructure;
 
+
 import java.util.Collection;
 import java.util.HashMap;
 
+
 public class DGraph implements graph{
-	HashMap <Integer,node_data> vertices;
-	HashMap <Integer,HashMap<Integer,edge_data>> edges;
-	int v;
+	HashMap <Integer,node_data> vertices; //pints
+	HashMap <Integer,HashMap<Integer,edge_data>> edges;//lines
+	int verticesCounter;
+	int edgesCounter;
+
+
+	public DGraph() {
+		vertices = new HashMap <Integer,node_data>();
+		edges = new HashMap <Integer,HashMap<Integer,edge_data>>();
+		verticesCounter = 0;
+		edgesCounter = 0;
+	}
+	
 	
 	@Override
 	public node_data getNode(int key) {
-		
 		if(vertices.containsKey(key)){
 			return vertices.get(key);
 		} else
 			return null;
 	}
 
+
 	@Override
 	public edge_data getEdge(int src, int dest) {
-		
 		if (edges.containsKey(src) && edges.get(src).containsKey(dest)) {
 			return edges.get(src).get(dest);
 		}
@@ -28,50 +39,68 @@ public class DGraph implements graph{
 		}
 	}
 
+	
 	@Override
 	public void addNode(node_data n) {
+		if (this.vertices == null) {
+			DGraph temp = new DGraph();
+			this.edges = temp.edges;
+			this.vertices = temp.vertices;
+			this.verticesCounter = temp.verticesCounter;
+		}
 		this.vertices.put(n.getKey(),n);
-		v++;
-
+		verticesCounter++;
 	}
 
+	
 	@Override
 	public void connect(int src, int dest, double w) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
+	
+	@SuppressWarnings("unchecked")
 	@Override
 	public Collection<node_data> getV() {
-		// TODO Auto-generated method stub
-		return null;
+		return (Collection<node_data>) this.vertices;
 	}
 
+	
+	@SuppressWarnings("unchecked")
 	@Override
 	public Collection<edge_data> getE(int node_id) {
-		// TODO Auto-generated method stub
-		return null;
+		return (Collection<edge_data>) this.edges.get(node_id);
 	}
 
+	
 	@Override
 	public node_data removeNode(int key) {
-		// TODO Auto-generated method stub
-		if (vertices.containsKey(key)) {
-			
+		if (this.vertices.containsKey(key)) {
+			node_data toReturn = this.vertices.remove(key);
+			this.edges.remove(key);
+			this.verticesCounter--;
+			return toReturn;
 		}
 		return null;
 	}
 
+	
 	@Override
 	public edge_data removeEdge(int src, int dest) {
-		// TODO Auto-generated method stub
+		if (this.edges.containsKey(src)) {
+			edge_data toReturn = this.edges.get(src).remove(dest);
+			if (toReturn != null) {
+				this.edgesCounter--;
+			}
+			return toReturn;
+		}
 		return null;
 	}
 
+	
 	@Override
 	public int nodeSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.verticesCounter;
 	}
 
 	@Override
