5761e8db083c1d7cb76a49fa9e6763e3c39542a5
Yarin Shapira <yariniii19@gmail.com>
2019-12-30 20:04:02 +0200

    fixing addNode

--- src/dataStructure/DGraph.java
+++ src/dataStructure/DGraph.java
@@ -53,9 +53,6 @@ public class DGraph implements graph{
 			this.vertices = temp.vertices;
 			this.verticeCounter = temp.verticeCounter;
 		}
-		if (this.getNode(n.getKey()) != null) {
-			throw new RuntimeException("the key you tryed to add already exists");
-		}
 		this.vertices.put(n.getKey(),n);
 		this.edges.put(n.getKey(),new HashMap<Integer,edge_data>());
 		verticeCounter++;
